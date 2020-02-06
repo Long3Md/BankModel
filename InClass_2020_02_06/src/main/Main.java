e32256de73381e6be2ffba924d8667427721a65d
mark <mark@courtneys-air.uccc.uc.edu>
2020-02-06 11:50:12 -0500

    Added objects to Main class

--- InClass_2020_02_06/src/main/Main.java
+++ InClass_2020_02_06/src/main/Main.java
@@ -1,10 +1,15 @@
 package main;
 
+import bank.Bank;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			//Declare and instansiate a Bank, an online, a BrickandMorter, and a CafeBranch object
+		Bank companyName = new Bank();
+		Bank URL = new Bank();
+		Bank Menu = new Bank();
+		Bank address = new Bank();
 	}
 
 }
